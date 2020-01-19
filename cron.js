fc7a7e4b1fb4e9f147f401af78df77ea55cc6f18
rodrigue <peniouatt@hotmail.fr>
2020-01-19 20:58:08 +0100

    fermeture popup

--- cron.js
+++ cron.js
@@ -1,7 +1,7 @@
 var sh = require('shelljs');
 var CronJob = require('cron').CronJob;
 console.log('debut instanciation');
-const job = new CronJob('* * * * * *', function() {
+const job = new CronJob('0 * * * * ', function() {
   sh.exec('casperjs ./index.js');
 });
 console.log('fin instanciation');
--- index.js
+++ index.js
@@ -24,84 +24,102 @@ casper.then(function(){
    this.click(x('//*[@id="nextButtonId"]'));
 })
 
+
+
 //boucler tant qu'on a la popin alert
+// casper.waitForAlert('remote.alert', 
+//     function(message) 
+//     { this.echo('alert message: ' + message);
+// });
 
-casper.then(function(){
-    // je dois trouver le path de l'alert
-    while(this.exists(/*xpath de la popin "aucun rdv disponible"*/))
-    {
-        // click sur le ok pour fermer l'alert
-        this.click(x(/*Xpath du bouton ok pour fermer la popin*/));
-        // click sur continuer 
-        this.click(x('//*[@id="nextButtonId"]'));
-    }
-})
+// casper.then(function(){
+//     // je dois trouver le path de l'alert
+//     while(this.exists(/*xpath de la popin "aucun rdv disponible"*/))
+//     {
+//         // click sur le ok pour fermer l'alert
+//         this.click(x(/*Xpath du bouton ok pour fermer la popin*/));
+//         // click sur continuer 
+//         this.click(x('//*[@id="nextButtonId"]'));
+//     }
+// })
 
-casper.then(function(){
-    while(this.exists('a.ui-state-default')){
+function repeatScript(){
+    casper.waitForAlert(function(response) {
+        repeatScript();
         
-    }
-})
-
-casper.then(function(){
-    casper.evaluate(function() {
-        openJqueryCal(true);
+    },function(){
+        nextStep();
     });
-})
+}
 
-casper.waitForSelector('a.ui-state-default', function(){
-    this.wait(1000, function() {
-        console.log('capture calendar');
-        this.capture('prefecture.png', {
-            top: 100,
-            left: 100,
-            width: 1200,
-            height: 800
+function nextStep(){
+    casper.then(function(){
+        while(this.exists('a.ui-state-default')){
+            
+        }
+    })
+    
+    casper.then(function(){
+        casper.evaluate(function() {
+            openJqueryCal(true);
+        });
+    })
+    
+    casper.waitForSelector('a.ui-state-default', function(){
+        this.wait(1000, function() {
+            console.log('capture calendar');
+            this.capture('prefecture.png', {
+                top: 100,
+                left: 100,
+                width: 1200,
+                height: 800
+            });
+        });
+    }, function () {
+        console.log('timeout');
+        this.exit();
+    });
+    
+    casper.then(function() {
+        this.click('a.ui-state-default');
+        this.wait(1000, function() {
+            console.log('capture clicked');
+            this.capture('clicked.png', {
+                top: 100,
+                left: 100,
+                width: 1200,
+                height: 800
+            });
         });
     });
-}, function () {
-    console.log('timeout');
-    this.exit();
-});
-
-casper.then(function() {
-    this.click('a.ui-state-default');
-    this.wait(1000, function() {
-        console.log('capture clicked');
-        this.capture('clicked.png', {
-            top: 100,
-            left: 100,
-            width: 1200,
-            height: 800
+    
+    casper.then(function(){
+        this.evaluate(function() {
+            document.querySelector('select#hourValueSelect').selectedIndex = 1; //it is obvious
+            disableButtons();
+            onHourChanged();
         });
     });
-});
-
-casper.then(function(){
-    this.evaluate(function() {
-        document.querySelector('select#hourValueSelect').selectedIndex = 1; //it is obvious
-        disableButtons();
-        onHourChanged();
+    
+    casper.waitForSelector('.availabilityColumnX', function () {
+        this.click('input[type="radio"]');
+        console.log('capture select');
+        this.capture('select.png');
     });
-});
-
-casper.waitForSelector('.availabilityColumnX', function () {
-    this.click('input[type="radio"]');
-    console.log('capture select');
-    this.capture('select.png');
-});
-
-casper.then(function(){
-    this.click(x('//*[@id="nextButtonId"]'));
-})
-
-casper.waitForSelector('.choice_captcha', function () {
-    console.log('capture form');
-    this.capture('form.png');
-
-    console.log('capture captcha');
-    this.captureSelector('captcha.png', '.img_captcha img');
-
-});
-
+    
+    casper.then(function(){
+        this.click(x('//*[@id="nextButtonId"]'));
+    })
+    
+    casper.waitForSelector('.choice_captcha', function () {
+        console.log('capture form');
+        this.capture('form.png');
+    
+        console.log('capture captcha');
+        this.captureSelector('captcha.png', '.img_captcha img');
+    
+    });
+    
+}
+repeatScript();
 casper.run(function(){});
